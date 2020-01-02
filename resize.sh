63d6167cf4e08124c3b1e9eb870d08779795bcde
Mrinalini Manandhar <mrinalini.manandhar@gmail.com>
2020-01-01 18:51:23 -0800

    Add auto-orient option to mogrify

--- resize.sh
+++ resize.sh
@@ -14,7 +14,7 @@ for f in ./renamed/*; do mogrify -path "./jpegs_100" -format jpeg "${f}"; done
 echo Successfully converted heics to jpegs...
 
 for f in ./jpegs_100/*.jpeg;
-  do echo Resizing $(basename "${f}") && mogrify -path "./resized" -filter Triangle -define filter:support=2 -thumbnail "50%" -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 85 -define heic:fancy-upsampling=off -define heic:compression-filter=5 -define heic:compression-level=9 -define heic:compression-strategy=1 -define heic:exclude-chunk=all -interlace none -colorspace sRGB "${f}";
+  do echo Resizing $(basename "${f}") && mogrify -auto-orient -path  "./resized" -filter Triangle -define filter:support=2 -thumbnail "50%" -unsharp 0.25x0.08+8.3+0.045 -dither None -posterize 136 -quality 85 -define heic:fancy-upsampling=off -define heic:compression-filter=5 -define heic:compression-level=9 -define heic:compression-strategy=1 -define heic:exclude-chunk=all -interlace none -colorspace sRGB "${f}";
 done
 
 echo Successfully resized images...
